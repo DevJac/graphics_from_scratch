6f7deb8a35b4804bd1e0122819c505cc30e11a77
DevJac <dbuttonz@gmail.com>
2023-08-11 17:23:19 -0600

    Open blank window with SDL2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let window = sdl_context
+        .video()
+        .unwrap()
+        .window("Renderer", 800, 600)
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    canvas.clear();
+    canvas.present();
+    'main_loop: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. }
+                | sdl2::event::Event::KeyDown {
+                    keycode: Some(sdl2::keyboard::Keycode::Escape),
+                    ..
+                } => break 'main_loop,
+                _ => {}
+            }
+        }
+        canvas.clear();
+        canvas.present();
+    }
 }
