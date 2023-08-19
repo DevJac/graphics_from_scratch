51abd0668649fa50a3a70ba8f32f05f4857dae16
DevJac <dbuttonz@gmail.com>
2023-08-18 18:02:09 -0600

    Use a color buffer, faster, draw grid lines

--- src/main.rs
+++ src/main.rs
@@ -1,117 +1,165 @@
-mod state {
-    // Unsafe: texture_creator and texture are related, the sdl2 docs aren't 100% clear on how.
-    // texture needs to be dropped before texture_creator (I think).
+use pixel_renderer::PixelRenderer;
+use sdl2::pixels::Color;
+
+mod pixel_renderer {
+    // Unsafe: TextureCreator and Texture are related, the sdl2 docs aren't 100% clear on how.
+    // color_texture needs to be dropped before texture_creator.
     // The plan is to keep both of them together in this struct, and to keep them private so
-    // that the unsafe behavior doesn't leak outside of this state module.
+    // that the unsafe behavior doesn't leak outside of this module.
     use sdl2::pixels::{Color, PixelFormat};
     use sdl2::render::{Canvas, Texture, TextureAccess, TextureCreator};
     use sdl2::video::{Window, WindowContext};
-    pub struct State {
+    use std::marker::PhantomPinned;
+    use std::pin::Pin;
+
+    struct PinWrapper<T> {
+        _pin: PhantomPinned,
+        value: T,
+    }
+
+    impl<T> PinWrapper<T> {
+        fn new(t: T) -> Self {
+            Self {
+                _pin: PhantomPinned,
+                value: t,
+            }
+        }
+    }
+
+    impl<T> std::ops::Deref for PinWrapper<T> {
+        type Target = T;
+
+        fn deref(&self) -> &Self::Target {
+            &self.value
+        }
+    }
+
+    pub struct PixelRenderer {
         pub width: u32,
         pub height: u32,
         pub context: sdl2::Sdl,
         pub canvas: Canvas<Window>,
-        // Unsafe: texture must be dropped before texture_creator.
+        // Unsafe: color_texture must be dropped before texture_creator.
         // Struct fields are dropped in the order they are defined,
-        // so texture must come before texture_creator in the struct definition.
-        color_buffer_texture: Box<Texture<'static>>,
-        texture_creator: std::pin::Pin<Box<TextureCreator<WindowContext>>>,
+        // so color_texture must come before texture_creator in the struct definition.
+        color_buffer: Box<[u8]>,
+        color_texture: Box<Texture<'static>>,
+        texture_creator: Pin<Box<PinWrapper<TextureCreator<WindowContext>>>>,
     }
 
-    impl State {
+    impl PixelRenderer {
         pub fn new(width: u32, height: u32) -> Self {
-            use std::ptr::addr_of_mut;
-
-            let mut result = std::mem::MaybeUninit::<Self>::uninit();
-            let rp = result.as_mut_ptr();
-            unsafe {
-                addr_of_mut!((*rp).width).write(width);
-                addr_of_mut!((*rp).height).write(height);
-                addr_of_mut!((*rp).context).write(sdl2::init().unwrap());
-                let window = (*rp)
-                    .context
-                    .video()
-                    .unwrap()
-                    .window("Renderer", width, height)
-                    .borderless()
-                    .maximized()
-                    .build()
-                    .unwrap();
-                addr_of_mut!((*rp).canvas).write(
-                    window
-                        .into_canvas()
-                        .accelerated()
-                        .present_vsync()
-                        .build()
-                        .unwrap(),
-                );
-                addr_of_mut!((*rp).texture_creator).write(Box::pin((*rp).canvas.texture_creator()));
-                addr_of_mut!((*rp).color_buffer_texture).write(Box::new(
-                    (*rp)
-                        .texture_creator
-                        .create_texture(
-                            (*rp).texture_creator.default_pixel_format(),
-                            TextureAccess::Streaming,
-                            width,
-                            height,
-                        )
-                        .unwrap(),
-                ));
-                result.assume_init()
+            let context = sdl2::init().unwrap();
+            let window = context
+                .video()
+                .unwrap()
+                .window("Renderer", width, height)
+                .borderless()
+                .maximized()
+                .build()
+                .unwrap();
+            let canvas = window
+                .into_canvas()
+                .accelerated()
+                .present_vsync()
+                .build()
+                .unwrap();
+            let pixel_count: usize = (width * height).try_into().unwrap();
+            let color_buffer: Box<[u8]> =
+                vec![0u8; pixel_count * std::mem::size_of::<Color>()].into_boxed_slice();
+            let texture_creator: Pin<Box<PinWrapper<TextureCreator<WindowContext>>>> =
+                Box::pin(PinWrapper::new(canvas.texture_creator()));
+            let tc_forget_lifetime: &Pin<Box<PinWrapper<TextureCreator<WindowContext>>>> =
+                unsafe { &*(&texture_creator as *const _) };
+            let color_texture = Box::new(
+                tc_forget_lifetime
+                    .create_texture(
+                        tc_forget_lifetime.default_pixel_format(),
+                        TextureAccess::Streaming,
+                        width,
+                        height,
+                    )
+                    .unwrap(),
+            );
+            Self {
+                width,
+                height,
+                context,
+                canvas,
+                color_buffer,
+                texture_creator,
+                color_texture,
             }
         }
 
         pub fn set_pixel(&mut self, x: u32, y: u32, c: Color) {
-            let soc: usize = std::mem::size_of::<Color>();
-            let width_u: usize = self.width.try_into().unwrap();
-            let pitch: usize = soc * width_u;
+            assert!(x < self.width);
+            assert!(y < self.height);
+            let size_of_color: usize = std::mem::size_of::<Color>();
+            let width: usize = self.width.try_into().unwrap();
+            let x: usize = x.try_into().unwrap();
+            let y: usize = y.try_into().unwrap();
+            let i = ((y * width) + x) * size_of_color;
 
-            let cb: &[u8] = &c
+            // Turn the color into bytes. The correct bytes for a color depend on
+            // the pixel format and the system endianness.
+            let color_bytes: &[u8; 4] = &c
                 .to_u32(
                     &PixelFormat::try_from(self.texture_creator.default_pixel_format()).unwrap(),
                 )
                 .to_ne_bytes();
 
-            self.color_buffer_texture
-                .update(
-                    sdl2::rect::Rect::new(x.try_into().unwrap(), y.try_into().unwrap(), 1, 1),
-                    cb,
-                    pitch,
-                )
-                .unwrap();
+            self.color_buffer[i..i + size_of_color].copy_from_slice(color_bytes);
         }
 
         pub fn render(&mut self) {
-            use rand::Rng;
-            self.canvas.set_draw_color(Color::RGB(255, 0, 255));
-            self.canvas.clear();
+            let size_of_color: usize = std::mem::size_of::<Color>();
+            let width: usize = self.width.try_into().unwrap();
+            let pitch: usize = width * size_of_color;
 
-            let mut rng = rand::thread_rng();
-            let rx = rng.gen_range(0..self.width);
-            let ry = rng.gen_range(0..self.height);
-            let rr = rng.gen_range(0..255);
-            let rg = rng.gen_range(0..255);
-            let rb = rng.gen_range(0..255);
-            self.set_pixel(rx, ry, Color::RGB(rr, rg, rb));
-
-            self.canvas
-                .copy(&self.color_buffer_texture, None, None)
+            self.color_texture
+                .update(None, &self.color_buffer, pitch)
                 .unwrap();
+
+            self.canvas.copy(&self.color_texture, None, None).unwrap();
 
             self.canvas.present();
         }
     }
 }
 
-fn main() {
-    use state::State;
+fn draw_grid(pixel_renderer: &mut PixelRenderer) {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    for x in 0..pixel_renderer.width {
+        for y in 0..pixel_renderer.height {
+            assert!(x < pixel_renderer.width);
+            assert!(y < pixel_renderer.height);
+            if x % 10 == 0 || y % 10 == 0 {
+                pixel_renderer.set_pixel(x, y, Color::RGB(100, 100, 0));
+            } else if x % 10 == 5 && y % 10 == 5 {
+                pixel_renderer.set_pixel(
+                    x,
+                    y,
+                    Color::RGB(
+                        rng.gen_range(0..255),
+                        rng.gen_range(0..255),
+                        rng.gen_range(0..255),
+                    ),
+                );
+            } else {
+                pixel_renderer.set_pixel(x, y, Color::RGB(0, 0, 0));
+            }
+        }
+    }
+}
 
+fn main() {
     let width = 3440 / 2;
     let height = 1440 / 2;
-    let mut state = State::new(width, height);
-    state.render();
+    let mut pixel_renderer = PixelRenderer::new(width, height);
     'main_loop: loop {
-        for event in state.context.event_pump().unwrap().poll_iter() {
+        for event in pixel_renderer.context.event_pump().unwrap().poll_iter() {
             match event {
                 sdl2::event::Event::Quit { .. }
                 | sdl2::event::Event::KeyDown {
@@ -121,6 +169,8 @@ fn main() {
                 _ => {}
             }
         }
-        state.render();
+
+        draw_grid(&mut pixel_renderer);
+        pixel_renderer.render();
     }
 }
