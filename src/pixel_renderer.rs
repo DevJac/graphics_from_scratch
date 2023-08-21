67dbdd61aaa6bbb62ee0fc4e901f2cbc9a577959
DevJac <dbuttonz@gmail.com>
2023-08-21 16:13:39 -0600

    Make SIZE_OF_COLOR const

--- src/pixel_renderer.rs
+++ src/pixel_renderer.rs
@@ -6,6 +6,8 @@ use sdl2::pixels::{Color, PixelFormat, PixelFormatEnum};
 use sdl2::render::{Canvas, Texture, TextureAccess, TextureCreator};
 use sdl2::video::{Window, WindowContext};
 
+const SIZE_OF_COLOR: usize = std::mem::size_of::<Color>();
+
 pub struct PixelRenderer {
     pub width: u32,
     pub height: u32,
@@ -47,8 +49,7 @@ impl PixelRenderer {
             .build()
             .unwrap();
         let pixel_count: usize = (width * height).try_into().unwrap();
-        let color_buffer: Box<[u8]> =
-            vec![0u8; pixel_count * std::mem::size_of::<Color>()].into_boxed_slice();
+        let color_buffer: Box<[u8]> = vec![0u8; pixel_count * SIZE_OF_COLOR].into_boxed_slice();
         // Unsafe: We will manage the life of texture_creator and color_texture ourselves.
         // We will keep them together in this struct and eventually drop them together.
         let texture_creator: *mut TextureCreator<WindowContext> =
@@ -76,23 +77,21 @@ impl PixelRenderer {
     pub fn set_pixel(&mut self, x: u32, y: u32, c: Color) {
         assert!(x < self.width);
         assert!(y < self.height);
-        let size_of_color: usize = std::mem::size_of::<Color>();
         let width: usize = self.width.try_into().unwrap();
         let x: usize = x.try_into().unwrap();
         let y: usize = y.try_into().unwrap();
-        let i = ((y * width) + x) * size_of_color;
+        let i = ((y * width) + x) * SIZE_OF_COLOR;
 
         // Turn the color into bytes. The correct bytes for a color depend on
         // the pixel format and the system endianness.
-        let color_bytes: &[u8; 4] = &c.to_u32(&self.pixel_format).to_ne_bytes();
+        let color_bytes: &[u8; SIZE_OF_COLOR] = &c.to_u32(&self.pixel_format).to_ne_bytes();
 
-        self.color_buffer[i..i + size_of_color].copy_from_slice(color_bytes);
+        self.color_buffer[i..i + SIZE_OF_COLOR].copy_from_slice(color_bytes);
     }
 
     pub fn render(&mut self) {
-        let size_of_color: usize = std::mem::size_of::<Color>();
         let width: usize = self.width.try_into().unwrap();
-        let pitch: usize = width * size_of_color;
+        let pitch: usize = width * SIZE_OF_COLOR;
 
         unsafe { &mut *self.color_texture }
             .update(None, &self.color_buffer, pitch)
